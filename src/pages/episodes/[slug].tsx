281b59c591f2e844032ec6fcd8a03d1e1fce79eb
Nicolas Theodoro <nicolastheodoro@gmail.com>
2021-04-25 21:50:25 -0300

    NLW Day 05

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -3,6 +3,9 @@ import ptBR from "date-fns/locale/pt-BR";
 import { GetStaticPaths, GetStaticProps } from "next"; 
 import Image from "next/image";
 import Link from "next/link";
+import Head from "next/head";
+
+import { usePlayer } from "../../contexts/PlayerContext";
 
 import { api } from "../../services/api";
 import { convertDurationToTimeString } from "../../utils/convertDurationToTimeString";
@@ -26,8 +29,12 @@ type EpisodeProps = {
 };
 
 export default function Episode({ episode }: EpisodeProps) {
+  const { play } = usePlayer();
   return (
     <div className={styles.episode}>
+      <Head>
+        <title>{episode.title} | Podcastr</title>
+      </Head>
       <div className={styles.thumbnailContainer}>
         <Link href="/">
           <button type="button">
@@ -42,7 +49,7 @@ export default function Episode({ episode }: EpisodeProps) {
         />
 
         <button type="button">
-          <img src="/play.svg" alt="Tocar episódio" />
+          <img src="/play.svg" alt="Tocar episódio" onClick={() => play(episode)}/>
         </button>
       </div>
 
